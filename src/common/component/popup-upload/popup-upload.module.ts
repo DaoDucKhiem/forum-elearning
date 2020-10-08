004359f3a5db08084c80ac3bb4c93b57a78ddbd2
DaoDucKhiem <daoduckhiem1104@gmail.com>
2020-10-08 22:35:45 +0700

    xay class chung

---
+++ src/common/component/popup-upload/popup-upload.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopupUploadComponent } from './popup-upload.component';
+
+
+
+@NgModule({
+  declarations: [PopupUploadComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PopupUploadModule { }
