838c1738e21d263b771098fb163b4b137f3a60f9
DaoDucKhiem <daoduckhiem1104@gmail.com>
2020-11-30 20:54:23 +0700

    sua domain production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   constructor(private userSV: UserService, private authenSV: AuthenService) { }
   ngOnInit() {
     this.initApp();
-   // this.getUserLogin();
+    //this.getUserLogin();
   }
 
   /**
