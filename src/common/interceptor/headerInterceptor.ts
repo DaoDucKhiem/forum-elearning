fc9d3fb4964baa64b164bd5604516b05010f1761
DaoDucKhiem <daoduckhiem1104@gmail.com>
2020-12-07 23:42:48 +0700

    sua header khi gui request len server stringee

--- src/common/interceptor/headerInterceptor.ts
+++ src/common/interceptor/headerInterceptor.ts
@@ -12,11 +12,14 @@ export class HeaderInterceptor implements HttpInterceptor {
         if (!token) {
             token = "";
         }
-        //token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJudmtoYWkyMDMiLCJ1c2VyX2lkIjoiMDhkODhhNTMtN2IyMC00ZGFjLTg2YTAtMmRkYjdlNjhiYjI4IiwiZW1haWwiOiJudmtoYWkyMDNAZ21haWwuY29tIiwibGlzdF9yb2xlcyI6InxTWVNfQURNSU4vxq_MgW5nIGR1zKNuZyBuaG_MgW0gMTJ8IiwibmJmIjoxNjA2Njc0MDE4LCJleHAiOjE2MDcyNzg4MTgsImlhdCI6MTYwNjY3NDAxOH0.CxoyZ8IKv80ffpNucDZVEHIqzt4_AuToOlxr3KmzLOA";
+        // token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJudmtoYWkyMDMiLCJ1c2VyX2lkIjoiMDhkODhhNTMtN2IyMC00ZGFjLTg2YTAtMmRkYjdlNjhiYjI4IiwiZW1haWwiOiJudmtoYWkyMDNAZ21haWwuY29tIiwibGlzdF9yb2xlcyI6InxTWVNfQURNSU4vxq_MgW5nIGR1zKNuZyBuaG_MgW0gMTJ8IiwibmJmIjoxNjA2Njc0MDE4LCJleHAiOjE2MDcyNzg4MTgsImlhdCI6MTYwNjY3NDAxOH0.CxoyZ8IKv80ffpNucDZVEHIqzt4_AuToOlxr3KmzLOA";
         // Clone the request to add the new header
-        const clonedRequest = req.clone({ headers: req.headers.append('Authorization', token) });
+        if (!req.headers.get("X-STRINGEE-AUTH")) {
+            const clonedRequest = req.clone({ headers: req.headers.append('Authorization', token) });
+            return next.handle(clonedRequest);
+        }
 
         // Pass the cloned request instead of the original request to the next handle
-        return next.handle(clonedRequest);
+        return next.handle(req);
     }
 }
