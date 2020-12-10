b7a9d2f29c78ee44779584c85eb8e5ea771131e0
duong17020661 <duong.coi.bs.1104@gmail.com>
2020-12-10 21:40:28 +0700

    xóa va sua tài lieu

--- src/app/component/user-management/user-management-content/document-management/document-management.component.ts
+++ src/app/component/user-management/user-management-content/document-management/document-management.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ParamDoc } from 'src/app/share/model/param/param-doc';
+import { DocumentService } from 'src/app/share/service/document.service';
 import { Customer, Service } from './app.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-document-management',
@@ -9,18 +13,109 @@ import { Customer, Service } from './app.service';
 })
 export class DocumentManagementComponent implements OnInit {
 
-  customers: Customer[];
   editVisible: boolean = false;
   deleteVisible: boolean = false;
-
-  constructor(service: Service) {
-      this.customers =  service.getCustomers();
+  listDocument: ParamDoc[] = [];
+  currentDocumentID: number;
+  curentDocument: ParamDoc;
+  isCategoryError: boolean;
+  isDocumentNameError = false;
+  isDescriptionError = false;
+  listCategory = [];
+  currentParam: ParamDoc = new ParamDoc();
+  description = "";
+  
+  constructor(private documentSV: DocumentService) {
   }
 
   ngOnInit(): void {
+    this.listCategory = [
+      {
+        ID: 1,
+        Name: "Giáo trình"
+      },
+      {
+        ID: 2,
+        Name: "Đề Thi"
+      },
+      {
+        ID: 3,
+        Name: "Tài liệu"
+      },
+      {
+        ID: 4,
+        Name: "Phương pháp"
+      },
+    ]
+    this.getDocumentByUserID();
+  }
+  getDocumentByUserID() {
+    this.documentSV.getDocumentByUserId("64a59a25-2488-54b0-f6b4-c8af08a50cbf").subscribe((res) => {
+      if (res?.Success) {
+        this.listDocument = res.Data;
+      }
+    })
   }
 
-  click(e){
-
+  click(e) {
+    this.deleteVisible = false;
+    this.editVisible = false;
+    if (e == 2) {
+      this.documentSV.deleteByID(this.currentDocumentID).subscribe((res) => {
+        if (res?.Success) {
+          this.getDocumentByUserID();
+        }
+      })
+    } if (e == 3) {
+      this.documentSV.updateDocument(this.curentDocument).subscribe((res) => {
+        if (res?.Success) {
+          this.getDocumentByUserID();
+        }
+      })
+    }
   }
+  deleteDocument(e) {
+    this.deleteVisible = true;
+    this.currentDocumentID = e.data.DocumentID;
+  }
+  editDocument(e){
+    this.curentDocument = e.data;
+    this.editVisible = true;
+  }
+
+  validateDescription(e) {
+    if (e) {
+      if (e.value && e.value.trim() != '') {
+        this.isDescriptionError = false;
+      }
+      else {
+        this.isDescriptionError = true;
+      }
+    }
+
+    this.curentDocument.Description = e.value;
+  }
+  setCategory(e) {
+    if (e) {
+      this.curentDocument.CategoryID = e.value;
+      this.isCategoryError = false;
+      $(".document-name").find("input").focus();
+    }
+  }
+
+  setDocumentName(e) {
+    if (e) {
+      this.curentDocument.DocumentName = e.value;
+      if (this.curentDocument.DocumentName && this.currentParam.DocumentName.trim() !== '') {
+        this.isDocumentNameError = false;
+      }
+    }
+  }
+
+  onEnterTextBox(e) {
+    if (e && e.event && e.event.keyCode === 13) {
+      $("dx-text-area").find("textarea").focus();
+    }
+  }
+  
 }
--- src/app/component/user-management/user-management-content/document-management/document-management.module.ts
+++ src/app/component/user-management/user-management-content/document-management/document-management.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentManagementComponent } from './document-management.component';
-import { DxButtonModule, DxDataGridModule, DxPopupModule } from 'devextreme-angular';
+import { DxButtonModule, DxDataGridModule, DxPopupModule, DxTextAreaModule } from 'devextreme-angular';
+import { PopupUploadModule } from 'src/common/component/popup-upload/popup-upload.module';
+import { SelectBoxModule } from 'src/app/share/module/select-box/select-box.module';
+import { TextBoxModule } from 'src/app/share/module/text-box/text-box.module';
 
 @NgModule({
   declarations: [DocumentManagementComponent],
@@ -9,7 +12,11 @@ import { DxButtonModule, DxDataGridModule, DxPopupModule } from 'devextreme-angu
     CommonModule,
     DxDataGridModule,
     DxPopupModule,
-    DxButtonModule
+    DxButtonModule,
+    PopupUploadModule,
+    DxTextAreaModule,
+    SelectBoxModule,
+    TextBoxModule
   ],
   exports: [DocumentManagementComponent]
 })
